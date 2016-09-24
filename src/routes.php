6ccf8556a57ebbac57814977e00a405998b67091
Gabriel Teles <gab.teles@hotmail.com>
2016-09-24 19:29:20 -0300

    [fix] fixing questions saving on database

--- src/routes.php
+++ src/routes.php
@@ -14,26 +14,32 @@ $app->post('/relatorio', function ($request, $response, $args) {
 
   $this->db->beginTransaction();
 
+  $this->logger->debug("Creating FORM: ");
   $form = Model::factory('FormSubmit')->create();
   $form->url = uniqid('');
   $form->created_at = time();
   $form->save();
+  $this->logger->debug("Form {$form->id}");
 
-  for ($i = 1 ; $i < 2; $i++) {
+  for ($i = 1 ; $i <= 25; $i++) {
     $fieldName = "q$i";
+    $this->logger->debug("Processing $fieldName");
 
     if (!isset($parsedBody[$fieldName])) {
+      $this->logger->debug("Field $fieldName not found");
       $this->db->rollBack();
       return $this->renderer->render($response, 'index.phtml');
     }
 
     $response_value = $parsedBody[$fieldName];
 
+    $this->logger->debug("Creating Question Response: ");
     $res = Model::factory('QuestionResponse')->create();
     $res->question_number = $i;
     $res->form_submit_id = $form->id;
     $res->response = $response_value;
     $res->save();
+    $this->logger->debug("Question Response {$res->id}");
   }
 
   $this->db->commit();
